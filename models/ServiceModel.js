5526d84f70980775229ecd85aa99d4c599d0ad73
unknown <product.tuzmo@gmail.com>
2022-09-27 00:58:00 +0500

    Model for service

--- models/ServiceModel.js
+++ models/ServiceModel.js
@@ -4,8 +4,23 @@ const Schema = mongoose.Schema;
 const serviceSchema = new Schema({
   name: { type: String },
   description: { type: String },
-  credits: { type: Number },
   isDeleted: { type: Boolean, default: false },
+
+  servicetype: [{ type: String, enum: ["IMEI", "SERVER"] }],
+  fieldType: {
+    type: { type: String, enum: ["SINGLE", "BOTH", "MULTIPLE", "CUSTOM"] },
+    customFields: [
+      {
+        name: String,
+        dataType: String, //NUMBERS, STRINGS
+      },
+    ],
+  },
+  credits: {
+    DEALER: { type: Number },
+    RESELLER: { type: Number },
+    USER: { type: Number },
+  },
 });
 const Service = mongoose.model("Service", serviceSchema);
 module.exports = Service;
